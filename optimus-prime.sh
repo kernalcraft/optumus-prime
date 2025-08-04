deccea06750b58f63f7f719ee023906d130902aa
kcraft <lkijh32@gmail.com>
2025-08-04 11:13:21 -0600

    Update optimus-prime.sh

--- optimus-prime.sh
+++ optimus-prime.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-echo "Optimus Prime is powering up... Coming soon!"
+echo "warming up power"
+#gather absolute path
+read -p "enter the path to the file you need cleaned up " filepath
+echo "you just entered the path $filepath"
+#check if the path exists
+if [[ -f $filepath ]]; then
+ BOOLEAN=true
+else
+ BOOLEAN=false
+fi
+echo "$BOOLEAN"
